81e9f25549240c027beabaaa7d039fa86e9aee3a
Yuening (Nicole) Lyu <63425078+nicolelyu021@users.noreply.github.com>
2021-11-10 19:25:29 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!--Harrison Weiss Nicole Lyu Nicole Salk -->
+<?php include 'view/header.php'; ?>
+<main>
+    <nav>
+        
+    <h2>Administrators</h2>
+    <ul>
+        <li><a href="manage_products/product_manager.php">Manage Products</a></li>
+        <li><a href="manage_technicians/manage_technicians.php">Manage Technicians</a></li>
+        <li><a href="manage_customers/SelectCustomerpage.php">Manage Customers</a></li>
+        <li><a href="errors/under_construction.php">Create Incident</a></li>
+        <li><a href="errors/under_construction.php">Assign Incident</a></li>
+        <li><a href="errors/under_construction.php">Display Incidents</a></li>
+    </ul>
+
+    <h2>Technicians</h2>    
+    <ul>
+        <li><a href="under_construction.php">Update Incident</a></li>
+    </ul> 
+
+    <h2>Customers</h2>
+    <ul>
+        <li><a href="manage_products/CustomerLogin.php">Register Product</a></li>
+    </ul>
+    
+    </nav>
+</section>
+<?php include 'view/footer.php'; ?>
---
+++ valuePullTest.php
@@ -0,0 +1,6 @@
+<?php
+$product_name = $_POST['product_name'];
+echo $product_name;
+$email = $_POST['email'];
+echo $email;
+?>
